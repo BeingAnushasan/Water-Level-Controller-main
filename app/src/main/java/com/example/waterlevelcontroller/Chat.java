9fa89beba85059f011cdda146b49255fde9ff6f1
Anushasan Poudel <beinganushasan@gmail.com>
2019-07-03 15:18:05 +0545

    first commit

---
+++ app/src/main/java/com/example/waterlevelcontroller/Chat.java
@@ -0,0 +1,214 @@
+package com.example.waterlevelcontroller;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.aflak.bluetooth.Bluetooth;
+
+public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCallback {
+    private String name;
+    private Bluetooth b;
+    private Button btnOn,btnOff;
+    private TextView text;
+    private ScrollView scrollView;
+    private boolean registered=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        text = findViewById(R.id.text);
+        btnOn =  findViewById(R.id.btnOn);
+        btnOff = findViewById(R.id.btnOff);
+        scrollView =  findViewById(R.id.scrollView);
+
+        text.setMovementMethod(new ScrollingMovementMethod());
+        btnOn.setEnabled(false);
+        btnOff.setEnabled(false);
+
+        b = new Bluetooth(this);
+        b.enableBluetooth();
+
+        b.setCommunicationCallback(this);
+
+        int pos = getIntent().getExtras().getInt("pos");
+        name = b.getPairedDevices().get(pos).getName();
+
+        Display("Connecting...");
+        b.connectToDevice(b.getPairedDevices().get(pos));
+
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = "a";
+                b.send(msg);
+                Display("You: "+msg);
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = "d";
+                b.send(msg);
+                Display("You: "+msg);
+            }
+        });
+
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
+        registered=true;
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(registered) {
+            unregisterReceiver(mReceiver);
+            registered=false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.close:
+                b.removeCommunicationCallback();
+                b.disconnect();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            case R.id.goToSelect:
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void Display(final String s){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.append(s + "\n");
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onConnect(BluetoothDevice device) {
+        Display("Connected to "+device.getName()+" - "+device.getAddress());
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                btnOn.setEnabled(true);
+                btnOff.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnect(BluetoothDevice device, String message) {
+        Display("Disconnected!");
+        Display("Connecting again...");
+        b.connectToDevice(device);
+    }
+
+    @Override
+    public void onMessage(String message) {
+
+        Display(name+": "+message);
+    }
+
+    @Override
+    public void onError(String message) {
+        Display("Error: "+message);
+    }
+
+    @Override
+    public void onConnectError(final BluetoothDevice device, String message) {
+        Display("Error: "+message);
+        Display("Trying again in 3 sec.");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.connectToDevice(device);
+                    }
+                }, 2000);
+            }
+        });
+    }
+
+
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                Intent intent1 = new Intent(Chat.this, MainActivity.class);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered=false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered=false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                }
+            }
+        }
+    };
+
+
+}
