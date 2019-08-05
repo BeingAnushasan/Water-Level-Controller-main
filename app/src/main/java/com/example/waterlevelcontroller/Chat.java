2dbdc200bacd2a78d3336b30d940a81108608840
Anushasan Poudel <beinganushasan@gmail.com>
2019-08-05 17:33:02 +0545

    Working model with new Animation

--- app/src/main/java/com/example/waterlevelcontroller/Chat.java
+++ app/src/main/java/com/example/waterlevelcontroller/Chat.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,16 +34,18 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
     private ScrollView scrollView;
     private boolean registered=false;
 
-    AnimationDrawable chargingAnimation;
+    //AnimationDrawable chargingAnimation;
 
     private ImageView levelViewer;
-    private  TextView textRexeived;
+    private  TextView textinfo;
 
+    public int msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
         viewFlipper= findViewById(R.id.view_flipper);
         text = findViewById(R.id.text);
         btnOn =  findViewById(R.id.btnOn);
@@ -50,13 +53,17 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
         scrollView =  findViewById(R.id.scrollView);
 
         text.setMovementMethod(new ScrollingMovementMethod());
-        btnOn.setEnabled(true);
-        btnOff.setEnabled(true);
+
+        btnOn.setEnabled(false);
+        btnOff.setEnabled(false);
 
         levelViewer = findViewById(R.id.levelViewer);
+        textinfo =  findViewById(R.id.textInfo);
 
 
-        //levelViewer.setBackgroundResource(R.drawable.charging_animation);
+
+
+       // levelViewer.setBackgroundResource(R.drawable.charging_animation);
        //chargingAnimation= (AnimationDrawable) levelViewer.getBackground();
 
         b = new Bluetooth(this);
@@ -80,6 +87,8 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
                 //chargingAnimation.start();
 
 
+                Toast.makeText(Chat.this, "Requested To Turn On Motor!", Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -89,6 +98,7 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
                 String msg = "b";
                 b.send(msg);
                 Display("You: "+msg);
+                Toast.makeText(Chat.this, "Requested To Turn Off Motor!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -122,7 +132,6 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
-        boolean isClicked ;
         switch (item.getItemId()) {
             case R.id.close:
                 b.removeCommunicationCallback();
@@ -152,14 +161,19 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
     @Override
     public void onConnect(BluetoothDevice device) {
         Display("Connected to "+device.getName()+" - "+device.getAddress());
+
         this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-//                btnOn.setEnabled(true);
-//                btnOff.setEnabled(true);
+
+                btnOn.setEnabled(true);
+                btnOff.setEnabled(true);
+
+
             }
         });
     }
+
 
     @Override
     public void onDisconnect(BluetoothDevice device, String message) {
@@ -172,51 +186,62 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
     public void onMessage(String message) {
 
         Display(name+": "+message);
-//        try {
-////            //set time in mili
-////            Thread.sleep(5000);
-////
-////        }catch (Exception e){
-////            e.printStackTrace();
-////        }
        levelViewDeciding(message);
 
     }
 
-    private void levelViewDeciding(String message) {
+    public void levelViewDeciding(String message) {
 
-        int msg = Integer.parseInt(message);
+        msg  = Integer.parseInt(message);
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
 
-        if(msg<20){
-          levelViewer.setImageResource(R.drawable.battery_20);
-         Display("Water level less than 20% ");
-        }
-       else if(msg>1 && msg<=20){
-            levelViewer.setImageResource(R.drawable.battery_20);
-            Display("Water level about 20% ");
-        }
-        else if(msg>20 && msg<=40){
-            levelViewer.setImageResource(R.drawable.battery_30);
-            Display("Water level is about 30% ");
-        }
-      else if(msg>40 && msg<=60){
-            levelViewer.setImageResource(R.drawable.battery_50);
-            Display("Water level is  50% ");
-        }
-        else if(msg>60 && msg<=80){
-            levelViewer.setImageResource(R.drawable.battery_60);
-            Display("Water level is 60%");
-        }
-        else if(msg>90 && msg<=100){
-            levelViewer.setImageResource(R.drawable.battery_100);
-            Display("Water level is 90% ");
-        }
+                if (msg < 10) {
+                    levelViewer.setImageResource(R.drawable.level_10);
+                    Display("Water level less than 10% ");
+                    textinfo.setText("Water level less than 10% ");
 
-        else if(msg>80 && msg<=90){
-            levelViewer.setImageResource(R.drawable.battery_90);
-            Display("Water level is 100% ");
-        }
+                } else if (msg >= 10 && msg <= 20) {
+                      levelViewer.setImageResource(R.drawable.level_20);
+                    Display("Water level about 20% ");
+                     textinfo.setText("Water level about 20% ");
+                } else if (msg > 20 && msg <= 30) {
+                      levelViewer.setImageResource(R.drawable.level_30);
+                    Display("Water level is about 30% ");
+                    textinfo.setText("Water level is about 30% ");
+                } else if (msg > 30 && msg <= 40) {
+                     levelViewer.setImageResource(R.drawable.level_40);
+                    Display("Water level is about 40%");
+                      textinfo.setText("Water level is about 40%");
+                } else if (msg > 40 && msg <= 50) {
+                     levelViewer.setImageResource(R.drawable.level_50);
+                    Display("Water level is about 50%");
+                     textinfo.setText("Water level is about 50%");
+                } else if (msg > 50 && msg <= 60) {
+                     levelViewer.setImageResource(R.drawable.level_60);
+                    Display("Water level is about 60%");
+                     textinfo.setText("Water level is about 60%");
+                } else if (msg > 60 && msg <= 70) {
+                     levelViewer.setImageResource(R.drawable.level_70);
+                    Display("Water level is about 70%");
+                     textinfo.setText("Water level is about 70%");
+                } else if (msg > 70 && msg <= 80) {
+                     levelViewer.setImageResource(R.drawable.level_80);
+                    Display("Water level is about 80%");
+                     textinfo.setText("Water level is about 80%");
+                } else if (msg > 80 && msg <= 90) {
+                    levelViewer.setImageResource(R.drawable.level_90);
+                    Display("Water level is about 90%");
+                     textinfo.setText("Water level is about 90%");
+                } else if (msg > 90) {
+                     levelViewer.setImageResource(R.drawable.level_100);
+                    Display("Water level is about 100%");
+                     textinfo.setText("Water level is about 100%");
+                }
 
+            }
+    });
     }
 
 
@@ -225,6 +250,7 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
     public void onError(String message) {
         Display("Error: "+message);
     }
+
 
     @Override
     public void onConnectError(final BluetoothDevice device, String message) {
@@ -243,6 +269,9 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
             }
         });
     }
+
+
+
 
 
 
